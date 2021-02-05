d08b7abefb62c56e42da72414dd610f309fa1647
Trystan May <trystanmay@gmail.com>
2021-02-04 19:50:25 -0800

    Merge branch 'develop' of https://github.com/Duke02/CS499_TeachGitRepo into develop
    # Conflicts:
    #	helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //this is technically C code that will not produce an error
 #include
 
@@ -6,14 +5,4 @@ int main() {
 	printf("Hello World!\n\n");
 }
 
-=======
-<<<<<<< HEAD
-#include <stdio.h>
-
-int main(void){
-cout << "Return to monke" << endl;
-}
-=======
 //this is technically C code that will not produce an error
->>>>>>> d9f3df957112f2cf8a6405290e6318bf985215ae
->>>>>>> feature/3-returntomonke
